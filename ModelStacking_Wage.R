f292917dd75ca3468f6988c37b4a65f465213737
behzadn <b.najafi87@gmail.com>
2015-12-29 12:15:27 +0100

    Updating the Modelstacking_Wage.R

--- ModelStacking_Wage.R
+++ ModelStacking_Wage.R
@@ -4,3 +4,6 @@ library(ggplot2);library(caret)
 Wage<-subset(Wage,seelct=-c(logwage))
 #let's create a building dataset and validation set.
 inBuild<-createDataPartition(y=Wage$wage, p=0.7, list=FALSE)
+validation<-Wage[-inBuild,];buildData<-Wage[inBuild,]
+InTrain<-createDataPartition(y=buildData$wage, p=0.7, list=FALSE)
+training<-buildData[InTrain,];testing<-buildData[-InTrain,];
